f629e4e0a05fa3f66740b89f50af6bee53a24619
Walter Castro <walterkstro@gmail.com>
2022-12-10 17:48:22 -0600

    Created effect skeleton while load data

---
+++ components/home/introduction/SectionIntroduction.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="py-16 bg-gradient-to-b from-dark to-darken relative
+                    md:py-24
+                    lg:py-40">
+        <HeroBackground class="absolute right-0 top-0 animate-pulse"/>
+        <LayoutContainer>
+            <div class="grid grid-cols-2 gap-x-4 relative
+                        md:grid-cols-6 md:gap-x-6
+                        lg:grid-cols-12 lg:grid-rows-3">
+                <slot name="titles"></slot>
+                <slot name="extract"></slot> 
+                <div class="col-span-2
+                            md:col-span-4
+                            lg:col-span-5 lg:row-start-3"><Button text="Ver mi perfil" :path="{name:'perfil'}"/></div>
+            </div>
+
+            <Technologies class="absolute -bottom-20 
+            bg-dark text-light rounded-lg py-4 -translate-x-1/2 left-1/2 max-w-3xl min-[320px] w-full
+            md:-bottom-16
+            lg:py-8"/>
+        </LayoutContainer>
+        
+    </section>
+</template>
+
+<script lang="ts" setup>
+    import HeroBackground from '~/assets/img/hero background.svg';
+
+    const LayoutContainer = defineAsyncComponent(()=>import('../../Layout/Container.vue'));
+    const Technologies = defineAsyncComponent(()=>import('~/components/home/introduction/Technologies.vue'));
+    defineProps(['pending'])
+    // defineProps({
+    //     name:String,
+    //     position:String,
+    //     extract:String,
+    //     greeting:String,
+    // })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,55 @@
 <template>
-    <Suspense>
-        <template #default><HomeIntroduction :="propertiesHomeIntroduction" /></template>
-        <template #fallback>Loading..</template>
-    </Suspense>
+    <SectionIntroduction v-if="pending">
+        <template #titles>
+            <div role="status" class="col-span-2 mb-8 animate-pulse
+                    md:col-span-4
+                    lg:col-span-5">
+                <div class="h-4 bg-gray-200 rounded-full dark:bg-light w-48"></div>
+                <div class="h-8 bg-gray-200 rounded-full dark:bg-light my-4"></div>
+                <div class="h-4 bg-gray-200 rounded-full dark:bg-light mb-8"></div>
+            </div>
+        </template>
+        <template #extract>
+            <div role="status" class="col-span-2 mb-4 text-light
+                                md:col-span-4
+                                lg:col-span-5 lg:row-start-2 animate-pulse">
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-light mb-2"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-light mb-2"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-light mb-2"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-light mb-2"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-light"></div>
+            </div>
+        </template>
+    </SectionIntroduction>
+    
+    <SectionIntroduction v-else>
+        <template #titles>
+            <div class="col-span-2 mb-8
+                    md:col-span-4
+                    lg:col-span-5">
+            <span class="block text-2xl md:text-3xl text-light">{{ data?.homePage.greeting }}</span>
+            <h1 class="font-bold text-orange">{{data?.homePage.name}}</h1>
+            <span class="block text-2xl md:text-3xl text-light">{{data?.homePage.position}}</span>
+        </div>
+        </template>
+        <template #extract>
+            <div class="col-span-2 mb-4 text-light
+                    md:col-span-4
+                    lg:col-span-5 lg:row-start-2"><p>{{ data?.homePage.extract }}</p></div>
+        </template>
+    </SectionIntroduction>
 </template>
-
 <script lang="ts" setup>
     import { HomePage } from '../Interfaces/Interfaces';
     import { homeQuery } from '../queries/homePage';
-    
-    const HomeIntroduction = defineAsyncComponent(()=>import('../components/Home/Introduction/Introduction.vue'))
+    import {SkeletonText} from 'skeleton-elements/vue';
+
+    import 'skeleton-elements/css/skeleton-elements.css'
+    const SectionIntroduction = defineAsyncComponent(()=>import('../components/home/introduction/SectionIntroduction.vue'))
 
     useHead({
         title:'Inicio'
     })
 
-    const { data,pending } = await useAsyncQuery<HomePage>(homeQuery)
-    const propertiesHomeIntroduction = {
-        name: data.value?.homePage.name,
-        position: data.value?.homePage.position,
-        extract: data.value?.homePage.extract,
-        greeting: data.value?.homePage.greeting
-    }
-    
+    const { data,pending } = await useLazyAsyncQuery<HomePage>(homeQuery); 
 </script>
