feafdf749e96326621602ab8dce22e726104cd94
Walter Castro <walterkstro@gmail.com>
2023-01-26 10:20:40 -0600

    Change style list in page home

--- components/home/areas/Area.vue
+++ components/home/areas/Area.vue
@@ -3,7 +3,7 @@
         <slot name="title"></slot>
         <ul class="list-none">
             <li v-for="item in items" :key="item" class="py-1">
-                <Icon name="bi:check-all"/> {{ item }}
+                <svg class="w-5 h-5 text-dark inline-block" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg> {{ item }}
             </li>
         </ul>
     </div>
