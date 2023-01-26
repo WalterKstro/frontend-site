bc272582876e51ecec977ec081ec4d43dbe9c22b
Walter Castro <walterkstro@gmail.com>
2023-01-26 10:56:46 -0600

    Setting favicon in file config of nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,5 +23,15 @@ export default defineNuxtConfig({
         public: {
             urlContacto: process.env.NUXT_URL_CONTACT,
         }
+    },
+    app: {
+        head: {
+            link: [
+                { rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"},
+                {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
+                {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
+                {rel:"manifest", href:"/site.webmanifest"},
+            ]
+        }
     }
 })
